25d04c3030d8a0f573e92304879c44339b44731d
Marc Chen <128506536+MarcChen@users.noreply.github.com>
2025-09-17 15:42:23 +0200

    fix: using correct exposed port on render webservice

--- main.py
+++ main.py
@@ -33,4 +33,5 @@ async def webhook_listener(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
