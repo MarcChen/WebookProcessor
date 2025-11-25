3aaaa2eb0b291bf3952ffd69da2fdc595c9e0d5b
Marc <128506536+MarcChen@users.noreply.github.com>
2025-11-25 21:41:25 +0100

    feat: add Terraform configuration for GitHub Actions secrets management and update .gitignore

---
+++ config/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {
+  owner = var.github_owner
+}
+
+locals {
+  raw_secrets = {
+    GMAIL_SERVICE_ACCOUNT_KEY = var.gmail_service_account_key
+    GMAIL_USER_EMAIL          = var.gmail_user_email
+    GMAIL_PUBSUB_TOPIC        = var.gmail_pubsub_topic
+    GMAIL_REFRESH_TOKEN       = var.gmail_refresh_token
+    GMAIL_CLIENT_ID           = var.gmail_client_id
+    GMAIL_CLIENT_SECRET       = var.gmail_client_secret
+  }
+
+  secrets = { for k, v in local.raw_secrets : k => v if v != "" }
+}
+
+resource "github_actions_secret" "repo_secrets" {
+  for_each        = local.secrets
+  repository      = var.github_repository
+  secret_name     = each.key
+  plaintext_value = each.value
+}
---
+++ config/variables.tf
@@ -0,0 +1,41 @@
+variable "github_owner" {
+  description = "The GitHub owner (user or organization) that owns the repository."
+  type        = string
+  default     = "MarcChen"
+}
+
+variable "github_repository" {
+  description = "The repository name where secrets will be created."
+  type        = string
+  default     = "WebookProcessor"
+}
+
+variable "gmail_service_account_key" {
+  description = "Gmail service account key JSON."
+  type        = string
+}
+
+variable "gmail_user_email" {
+  description = "Gmail user email to impersonate."
+  type        = string
+}
+
+variable "gmail_pubsub_topic" {
+  description = "Gmail Pub/Sub topic name."
+  type        = string
+}
+
+variable "gmail_refresh_token" {
+  description = "Gmail refresh token."
+  type        = string
+}
+
+variable "gmail_client_id" {
+  description = "Gmail client ID."
+  type        = string
+}
+
+variable "gmail_client_secret" {
+  description = "Gmail client secret."
+  type        = string
+}
